4f51cac417940e400d956da293e763e58693a209
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2021-03-14 15:31:25 +0530

    Update requestOperations.inc.php

--- includes/requestOperations.inc.php
+++ includes/requestOperations.inc.php
@@ -12,6 +12,7 @@ if(isset($_POST["submit"])) {
 
     if ($action === "error") {
         header("Location: ../view.php?id=".$postId);
+        exit();
     }
 
     $status = $action."-".$uid."-".$name;
