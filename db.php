b97ef0ea70fc74b805a70d9447694ae7c0f261bd
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2021-03-17 14:06:37 +0530

    delete

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$serverName = "localhost";
-$dBUserName = "root";
-$dBPassword = "";
-$dBName = "reqSys1";
-
-$con = mysqli_connect($serverName, $dBUserName, $dBPassword, $dBName);
-$conn = $con;
