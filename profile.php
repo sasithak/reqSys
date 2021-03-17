f0321812f38ab77bb5e667704838a5b27ab88d80
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2021-03-17 13:39:49 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -38,7 +38,7 @@ if (isset($_GET['id'])){
         </table>
     </header>
         </br>
-        <section><h1>'.$displayName.'</h1></section>
+        <section class="head-name"><h1>'.$displayName.'</h1></section>
         
         <section class="userData">
             <table style="text-align:left">
