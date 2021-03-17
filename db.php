7a92f8fd05fc5cb5b42766b0fb28d8da9192893c
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2021-03-17 14:02:33 +0530

    test

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+
+$serverName = "localhost";
+$dBUserName = "root";
+$dBPassword = "";
+$dBName = "reqSys1";
+
+$con = mysqli_connect($serverName, $dBUserName, $dBPassword, $dBName);
+$conn = $con;
